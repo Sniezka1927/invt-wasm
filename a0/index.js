bc7a64ba3b2fda780404ab4870f0c88b34230cee
Sniezka <dygons14@gmail.com>
2023-12-27 12:01:35 +0100

    Fixed index.js

--- a0/index.js
+++ a0/index.js
@@ -1,4 +1,4 @@
-import { D, add, big_mul, from_integer, mul_up, multiply } from "./pkg/invt.js";
+import { D, add, big_mul } from "./pkg/invt.js";
 
 const init = async () => {
   {
@@ -18,28 +18,28 @@ const init = async () => {
       let addition = add(a, b);
       console.log(`Decimal add result = ${addition.v}`);
     }
-    {
-      let a = new D(BigInt(10));
-      let b = new D(BigInt(5));
-      let result = multiply(a, b);
-      console.log(`Decimal multiply result = ${result.v}`);
-    }
+    // {
+    //   let a = new D(BigInt(10));
+    //   let b = new D(BigInt(5));
+    //   let result = multiply(a, b);
+    //   console.log(`Decimal multiply result = ${result.v}`);
+    // }
     {
       let a = new D(BigInt(10));
       let b = new D(BigInt(5));
       let big_mul_result = big_mul(a, b);
       console.log(`Decimal big_mul result = ${big_mul_result.v}`);
     }
-    {
-      let a = new D(BigInt(10));
-      let b = new D(BigInt(5));
-      let mul_up_result = mul_up(a, b);
-      console.log(`Decimal mul_up result = ${mul_up_result.v}`);
-    }
-    {
-      let from_integer_result = from_integer(BigInt(10));
-      console.log(`Decimal from_integer result = ${from_integer_result.v}`);
-    }
+    // {
+    //   let a = new D(BigInt(10));
+    //   let b = new D(BigInt(5));
+    //   let mul_up_result = mul_up(a, b);
+    //   console.log(`Decimal mul_up result = ${mul_up_result.v}`);
+    // }
+    // {
+    //   let from_integer_result = from_integer(BigInt(10));
+    //   console.log(`Decimal from_integer result = ${from_integer_result.v}`);
+    // }
   }
 };
 
