ea1ea9501284793ac0bd10bb520fcf7b51d082d6
Sniezka <dygons14@gmail.com>
2023-12-28 09:27:20 +0100

    update

--- serde-cspr/front/index.ts
+++ serde-cspr/front/index.ts
@@ -3,16 +3,14 @@ import {
   SqrtPrice,
   get_custom_struct,
   get_delta_y,
-  receive_example_from_js,
-  send_example_to_js,
 } from "../pkg/invt.js";
 
 const init = async () => {
   // let decimalInstance = new D(125);
-  let received = send_example_to_js();
-  console.log(received);
-  let result = receive_example_from_js(received);
-  console.log(result);
+  // let received = send_example_to_js();
+  // console.log(received);
+  // let result = receive_example_from_js(received);
+  // console.log(result);
   // Handling structs
   {
     let sqrtPrice: SqrtPrice = {
@@ -24,18 +22,18 @@ const init = async () => {
   {
     // get delta y
     // 1073741824
-    // let sqrtPriceA: SqrtPrice = {
-    //   v: "234_878_324_943_782_000_000_000_000",
-    // };
-    // let sqrtPriceB: SqrtPrice = { v: "87_854_456_421_658_000_000_000_000" };
-    // let liquidity: Liquidity = { v: "983_983_249_092" };
     let sqrtPriceA: SqrtPrice = {
-      v: "1073741824",
+      v: "234878324943782000000000000",
     };
-    let sqrtPriceB: SqrtPrice = { v: "32768" };
-    let liquidity: Liquidity = { v: "1048576" };
+    let sqrtPriceB: SqrtPrice = { v: "87854456421658000000000000" };
+    let liquidity: Liquidity = { v: "983983249092" };
     let delta_y_up = get_delta_y(sqrtPriceA, sqrtPriceB, liquidity, true);
+    let delta_y_down = get_delta_y(sqrtPriceA, sqrtPriceB, liquidity, false);
     console.log(delta_y_up);
+    console.log(delta_y_down);
+    // // 144669023.842474597804911408
+    // assert_eq!(result_down, TokenAmount::new(U256::from(1446690238)));
+    // assert_eq!(result_up, TokenAmount::new(U256::from(1446690239)));
   }
 };
 
